e9371e0c1ade2302411e9512e80a28d9074fb8e5
gitJaesik <maguire1815@gmail.com>
2018-08-30 11:34:14 +0900

    Upload Test - Simple code for CMake build system test

---
+++ Includes/Test.h
@@ -0,0 +1,12 @@
+//
+// Created by magui on 2018-08-30.
+//
+
+#ifndef SERVERCPP_TEST_H
+#define SERVERCPP_TEST_H
+
+#include <iostream>
+
+int Add(int a, int b);
+
+#endif //SERVERCPP_TEST_H
---
+++ Sources/Test.cpp
@@ -0,0 +1,11 @@
+//
+// Created by magui on 2018-08-30.
+//
+
+#include <Test.h>
+
+int Add(int a, int b)
+{
+    return a + b;
+}
+
