f310939b7c72430024920c9ef65ea9e592ec1f09
gitJaesik <maguire1815@gmail.com>
2018-09-07 21:27:55 +0900

    Upload unitTests - Main entry of UnitTests

---
+++ Tests/UnitTests/UnitTests.cpp
@@ -0,0 +1,14 @@
+//
+// Created by maguire1815 on 07/09/2018.
+//
+
+#include <gtest/gtest.h>
+
+int main(int argc, char * argv[])
+{
+    ::testing::InitGoogleTest(&argc, argv);
+
+    int ret = RUN_ALL_TESTS();
+
+    return ret;
+}
